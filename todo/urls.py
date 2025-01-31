a526b0b3a085a0b4a21b1e9021786019bfe23fea
Maksym <chukhno.maksym@gmail.com>
2025-01-31 13:55:47 +0200

    Creating styles

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
-from asyncio import tasks
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from todo.views import (TaskListView,
@@ -15,15 +15,15 @@ from todo.views import (TaskListView,
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="tasks_list"),
-    path("create/", TaskCreateView.as_view(), name="task_form"),
-    path("update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
-    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
+    path("task/create/", TaskCreateView.as_view(), name="task_form"),
+    path("task/update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
+    path("task/delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
     path("complete/<int:pk>/", TaskCompleteView.as_view(), name="task_complete"),
     path("undo/<int:pk>", TaskUndoView.as_view(), name="task_undo"),
     path("tags/", TagListView.as_view(), name="tags_list"),
     path("tags/create/", TagCreateView.as_view(), name="tag_form"),
     path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
     path("tags/delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "todo"
